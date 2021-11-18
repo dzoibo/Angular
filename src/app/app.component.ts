bac00ba08ec964b928a59a2cdfce65f3bf0939c6
dzoibo <87028108+dzoibo@users.noreply.github.com>
2021-11-18 15:10:06 +0100

    commit avec les promises et l'erreur de reject toujours inconues

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,30 +7,37 @@ import { Component } from '@angular/core';
 })
 export class AppComponent 
 {
-  isAuth=false;
- 
-  appareils = [
-    {
-      name: 'Machine à laver',
-      status: 'éteint'
-    },
-    {
-      name: 'Frigo',
-      status: 'allumé'
-    },
-    {
-      name: 'Ordinateur',
-      status: 'éteint'
+    isAuth=false;
+    lastUpdate = new Promise((resolve, reject) => {
+      const date = new Date();
+      setTimeout(
+        () => {
+          resolve(date);
+        }, 2000
+      );
+    }); /*last update est une Promise qui est une pipe modifiée*/
+    appareils = [
+      {
+        name: 'Machine à laver',
+        status: 'éteint'
+      },
+      {
+        name: 'Frigo',
+        status: 'allumé'
+      },
+      {
+        name: 'Ordinateur',
+        status: 'éteint'
+      }
+    ];
+
+    
+    constructor() {
+      setTimeout(()=> {this.isAuth=true;} ,4000
+      );
     }
-  ];
 
-  
-  constructor() {
-    setTimeout(()=> {this.isAuth=true;} ,4000
-    );
+    onAllumer() {
+      console.log('On allume tout !');
   }
-
-  onAllumer() {
-    console.log('On allume tout !');
-}
 }
